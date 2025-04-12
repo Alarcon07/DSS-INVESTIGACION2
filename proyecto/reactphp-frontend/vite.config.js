c43a1b81dfb192fbc25064b0fef26bf460f18996
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-04-11 23:12:19 -0600

    funcion de frontend

--- proyecto/reactphp-frontend/vite.config.js
+++ proyecto/reactphp-frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,              // Exponer para poder acceder desde fuera del contenedor
+    port: 5173,              // El mismo puerto que exponés en docker-compose
+    strictPort: true,
+    watch: {
+      usePolling: true       // Importante dentro de Docker para que los cambios se detecten bien
+    },
+    fs: {
+      strict: false
+    },
+    historyApiFallback: true // 🔥 Esto soluciona el error 404 al refrescar rutas
+  }
 })
